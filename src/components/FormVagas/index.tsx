57b475afbfd634975feae81571c896db056b9a97
Raphael Rodrigues <raphaelrodrigues_flk@hotmail.com>
2024-10-15 04:42:10 -0300

    fix: as prop no FormVagas

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+import { BtnPesquisar, Form, Campo } from './styles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +15,17 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Form as="form" onSubmit={aoEnviarForm}>
+      <Campo
+        as="input"
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
+      <BtnPesquisar as="button" type="submit">
         Pesquisar
-      </button>
-    </form>
+      </BtnPesquisar>
+    </Form>
   )
 }
 export default FormVagas
