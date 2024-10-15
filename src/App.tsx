bb60233027f8af8f9ce3743d9a299d9027c86db1
Raphael Rodrigues <raphaelrodrigues_flk@hotmail.com>
2024-10-15 03:48:21 -0300

    instalando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
 import './global.css'
+import temaStandard from './themes/standard'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaStandard}>
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
